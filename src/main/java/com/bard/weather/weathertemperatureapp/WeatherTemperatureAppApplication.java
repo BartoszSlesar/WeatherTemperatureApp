7249b687a61b51b0e0ba024c347c7624d21070b1
bard <bartoszslesar@gmail.com>
2024-06-15 21:09:02 +0200

    project initialization

---
+++ src/main/java/com/bard/weather/weathertemperatureapp/WeatherTemperatureAppApplication.java
@@ -0,0 +1,13 @@
+package com.bard.weather.weathertemperatureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTemperatureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTemperatureAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bard/weather/weathertemperatureapp/WeatherTemperatureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bard.weather.weathertemperatureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherTemperatureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
